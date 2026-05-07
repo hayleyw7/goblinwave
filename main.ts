032ec8ee7c9d5b6937c074aac4fccb67426983d7
Hayley Witherell <hayleywitherell@gmail.com>
2026-05-06 20:39:48 -0600

    Add single goblin counterattack example

--- main.ts
+++ main.ts
@@ -28,8 +28,12 @@ function randomDamage(max: number): number {
 
 const sampleHit = randomDamage(player.attack);
 goblin.hp = goblin.hp - sampleHit;
+const goblinHit = randomDamage(goblin.attack);
+player.hp = player.hp - goblinHit;
 
 console.log("Player:", player);
 console.log("Enemy:", goblin);
 console.log(`You hit the goblin for ${sampleHit} damage.`);
 console.log(`Goblin HP: ${goblin.hp}`);
+console.log(`Goblin hits you for ${goblinHit} damage.`);
+console.log(`Player HP: ${player.hp}`);
